db4871ea55c78df946a0635d7d8292e3e0fd5669
Jeff Fredrickson <jeff.fredrickson@gmail.com>
2015-02-02 16:42:42 -0500

    add convenience variable for trigger element

--- sandbox.js
+++ sandbox.js
@@ -5,21 +5,23 @@
   
   $(document).ready(function () {
     var $panel = $(".panel");
+    var $trigger = $(".trigger");
+    
     $panel.hide();
     
-    $(".trigger").bind("touchstart mouseover", function (event) {
+    $trigger.bind("touchstart mouseover", function (event) {
       console.log("event: " + event.type);
       $panel.show();
       event.preventDefault();
     });
     
-    $(".trigger").bind("mouseout", function (event) {
+    $trigger.bind("mouseout", function (event) {
       console.log("event: " + event.type);
       $panel.hide();
       event.preventDefault();
     });
     
-    $(".trigger").click(function (event) {
+    $trigger.click(function (event) {
       console.log("event: " + event.type);
     });
     
