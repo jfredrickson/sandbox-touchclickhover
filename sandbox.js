501f9e873844492bb4a6bea09cdc9aae4c36d76c
Jeff Fredrickson <jeff.fredrickson@gmail.com>
2015-09-28 12:15:21 -0400

    handle touch state

--- sandbox.js
+++ sandbox.js
@@ -2,29 +2,37 @@
 
 (function () {
   "use strict";
-  
+
   $(document).ready(function () {
     var $panel = $(".panel");
     var $trigger = $(".trigger");
-    
+    var active = false;
+
     $panel.hide();
-    
-    $trigger.bind("touchstart mouseover", function (event) {
+
+    $trigger.bind("touchstart", function (event) {
+      console.log("event: " + event.type);
+      active ? $panel.hide() : $panel.show();
+      active = !active;
+      event.preventDefault();
+    });
+
+    $trigger.bind("mouseover", function (event) {
       console.log("event: " + event.type);
       $panel.show();
       event.preventDefault();
     });
-    
+
     $trigger.bind("mouseout", function (event) {
       console.log("event: " + event.type);
       $panel.hide();
       event.preventDefault();
     });
-    
+
     $trigger.click(function (event) {
       console.log("event: " + event.type);
     });
-    
+
   });
-  
+
 }());
